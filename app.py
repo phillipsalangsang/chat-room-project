2e1dd6fbcd3ba8f33a93e2b2b11b3ff3fec71ce3
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-03-27 16:50:57 +0100

    Routes created POST GET

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "PhillipLouis"
 socketio = SocketIO(app)
 
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
