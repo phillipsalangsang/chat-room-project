aa2f1de100f8da2aebd5bae99e52b3aa0188a526
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-03-28 15:57:43 +0100

    Added redirect to room.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -44,8 +44,17 @@ def home():
 
         session["room"] = room
         session["name"] = name
+        return redirect(url_for("room"))
 
     return render_template("home.html")
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
